7b33a22efc9b32b7d2928d6bf4cdb66c167b0efd
Pankaj Kumar <forpankaj.prasar@gmail.com>
2020-11-08 16:41:23 +0530

    First Version of Our App

---
+++ src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function MyInfo(){
+    return(
+        <div>
+            <h1>Pankaj Kumar</h1>
+            <p>I am Pankaj Kumar a Software Developer More Details is added</p>
+            <ul>
+                <li>Shri Lanka</li>
+                <li>Munar</li>
+                <li>Fort Kochi</li>
+                <li>From Components</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
